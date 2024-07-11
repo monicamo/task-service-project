28c9974b8bc1703ea226d3cb5d345d62fb4ad93e
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-11 19:21:11 -0300

    Using The Alternative Dependency Injection Syntax

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -9,4 +10,9 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
 })
-export class TasksComponent {}
+export class TasksComponent {
+
+  constructor(private tasksService: TasksService) {
+
+  }
+}
