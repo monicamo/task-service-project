a241a3ebf7819ad4db7b310865148900cba87b42
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-12 08:22:12 -0300

    Outsourcing & Reusing Logic with Services

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -11,12 +11,31 @@ import { TasksService } from '../tasks.service';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
+  private selectedFilter = signal<string>('all');
   private tasksService = inject(TasksService);
-  tasks = this.tasksService.allTasks;
+
+  tasks = computed( () => {
+    switch(this.selectedFilter()) {
+      case 'all':
+        return this.tasksService
+          .allTasks();
+      case 'open':
+        return this.tasksService.allTasks()
+        .filter((task) => task.status === 'OPEN');;
+      case 'in-progress':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
-
   }
 }
